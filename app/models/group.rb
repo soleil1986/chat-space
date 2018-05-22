eb1540b7fdf8380070e0f05950847304cd90df3d
hiroaki <sanotaiyou@sanotaiyounoMacBook-puro.local>
2018-05-22 17:40:24 +0900

    add last message
    最新の投稿が出るようにした。

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,15 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :group_users
   has_many :users, through: :group_users
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.body?
+       last_message.body
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
