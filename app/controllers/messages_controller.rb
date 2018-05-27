073880b3730219137955d3b239ec81348819aeaf
hiroaki <sanotaiyou@sanotaiyounoMacBook-puro.local>
2018-05-27 12:40:09 +0900

    [WIP]チャット画面自動更新機能実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,21 +4,25 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json{@new_messages = @messages.where('id > ?', params[:id]) }
+    end
   end
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
+    if  @message.save
         respond_to do |format|
           format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
           format.json
-    end
+        end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
+    end
   end
-end
 
 private
 
@@ -29,4 +33,5 @@ private
   def set_group
     @group = Group.find(params[:group_id])
   end
+
 end
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,5 @@
 json.body  @message.body
 json.user_name  @message.user.name
 json.image  @message.image.url
-json.created_at  @message.created_at
+json.created_at  @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
+json.id @message.id
