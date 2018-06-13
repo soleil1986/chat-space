dfaabde3c22cd364ac62b4569116a3f0d8c09ec8
hiroaki <>
2018-06-13 14:48:59 +0900

    modify keys name

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/sanotaiyou.ssh/key_bird.pem']
+                  keys: ['/Users/sanotaiyou/.ssh/keybird.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
